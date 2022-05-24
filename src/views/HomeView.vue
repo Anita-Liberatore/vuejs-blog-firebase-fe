0216b50d596e2e048821dea79183920a8a4f2322
Anita Liberatore <anitaliberatore@hotmail.com>
2022-05-24 09:33:45 +0200

    clean code

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,15 @@
 <template>
   <main>
-    <HelloWorld />
+    
   </main>
 </template>
 
 <script>
-import HelloWorld from '../components/HelloWorld.vue';
 
 export default {
   components: {
-    HelloWorld
-  },
+    
+  }
 
 };
 </script>
